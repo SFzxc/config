8e3bf548f119b81f8b01ffd7556dc55ee0f2eb8f
SFzxc <hoangphuocvanlong@gmail.com>
2022-10-14 22:11:26 +0700

    fix: update lspconfig

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -1,7 +1,7 @@
 -- Set up nvim-cmp.
 local cmp = require('cmp')
 local lspconfig = require('lspconfig')
-local servers = { 'tsserver', 'ruby_ls', 'solidity', 'solargraph', 'dockerls' }
+local servers = { 'tsserver', 'solidity_ls', 'solargraph', 'dockerls' }
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
@@ -20,7 +20,6 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         local entry = cmp.get_selected_entry()
